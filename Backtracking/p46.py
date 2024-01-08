f76147bb146642fff5e552febe75310a0e958c55
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-01-08 02:44:23 +0000

    datily start

--- Backtracking/p46.py
+++ Backtracking/p46.py
@@ -1,4 +1,21 @@
 from typing import List
+
+
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        
+    def p(self, nums: List[int]) -> List[List[int]]:
+        result, remain_permute = [], []
+        total_len = len(nums)
+        current_result = []
+        while True:
+            current = nums.pop(0)
+            if nums:
+                remain_permute.append(nums)
+            current_result.append(current)
+            if len(current_result) == total_len:
+                result.append(current_result)
+                if remain_permute
+
+
+nums = [0, 1]
+a = Solution()
+print(a.permute(nums))
