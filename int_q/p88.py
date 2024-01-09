055ffdd56d4369347259aca9d3ae336cc44a279d
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-01-09 08:01:31 +0000

    123

--- int_q/p88.py
+++ int_q/p88.py
@@ -6,24 +6,6 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        pointer1, pointer2 = 0, 0
-
-        while pointer2 < n or pointer1 < m + n - 1:
-            if nums1[pointer1] == 0:
-                nums1.insert(pointer1, nums2[pointer2])
-                pointer2 += 1
-            else:
-                if nums2[pointer2] <= nums1[pointer1]:
-                    nums1.insert(pointer1, nums2[pointer2])
-                    pointer2 += 1
-                else:
-                    pointer1 += 1
-
-        for i in reversed(range(len(nums1))):
-            if i <= m + n - 1:
-                break
-            else:
-                nums1.pop(i)
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
@@ -31,11 +13,12 @@ m = 3
 nums2 = [2, 5, 6]
 n = 3
 
-
+"""
 nums1 = [2, 0]
 m = 1
 nums2 = [1]
 n = 1
+"""
 a = Solution()
 a.merge(nums1, m, nums2, n)
 print(nums1)
