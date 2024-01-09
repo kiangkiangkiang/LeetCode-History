b90e1111ca1d8153e1d0bd4f5013519ca4577adb
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-01-09 08:17:40 +0000

    123

--- int_q/p88.py
+++ int_q/p88.py
@@ -6,6 +6,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        for _ in range(len(nums1) - m):
+            nums1.pop()
+
+        if nums2:
+            nums1.extend(nums2)
+
+        nums1.sort()
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
@@ -13,12 +20,12 @@ m = 3
 nums2 = [2, 5, 6]
 n = 3
 
-"""
+
 nums1 = [2, 0]
 m = 1
 nums2 = [1]
 n = 1
-"""
+
 a = Solution()
 a.merge(nums1, m, nums2, n)
 print(nums1)
