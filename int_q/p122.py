1006533766050384b699452533986840e43da165
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-01-12 07:08:56 +0000

    interview

---
+++ int_q/p122.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+# However, you can buy it then immediately sell it on the same day.
+
+# Find and return the maximum profit you can achieve.
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # div_prices = [prices[i + 1] - prices[i] for i in range(len(prices) - 1) if prices[i + 1] - prices[i] > 0]
+        return sum([prices[i + 1] - prices[i] for i in range(len(prices) - 1) if prices[i + 1] - prices[i] > 0])
+        # print(div_prices)
+
+
+prices = [7, 1, 5, 3, 6, 4]
+a = Solution()
+print(a.maxProfit(prices))
