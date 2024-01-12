dcd2a6ffaf95c50e0bf4ce4bc9c234461e6f2e6a
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-01-12 05:53:13 +0000

    interview

--- int_q/p80.py
+++ int_q/p80.py
@@ -8,16 +8,22 @@ class Solution:
         counter = 0
         current_accumulate = 0
         while counter < len(nums):
+            if nums[counter] == prev:
+                current_accumulate += 1
+            else:
+                current_accumulate = 1
+
+            prev = nums[counter]
+
             if current_accumulate > 2:
                 nums.pop(counter)
             else:
-                if nums[counter] == prev:
-                    current_accumulate += 1
-                else:
-                    current_accumulate = 1
                 counter += 1
 
+        return len(nums)
 
-nums = [1, 1, 2, 3, 4]
+
+nums = [1, 1, 1]
 a = Solution()
 print(a.removeDuplicates(nums))
+print(nums)
