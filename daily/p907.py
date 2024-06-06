66066506e1c1baefaf112e4397c066e89603a14a
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-06-06 09:23:12 +0000

    daily

--- daily/p907.py
+++ daily/p907.py
@@ -3,9 +3,25 @@ from typing import List
 
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        memory = {}
+        # min_list = []
+        total = 0
         for start in range(len(arr)):
-            pass
+            last = None
+            for end in range(start + 1, len(arr) + 1):
+                current = arr[start:end][-1]
+                if last:
+                    if current < last:
+                        # min_list.append(current)
+                        total += current
+                        last = current
+                    else:
+                        # min_list.append(last)
+                        total += last
+                else:
+                    # min_list.append(current)
+                    total += current
+                    last = current
+        return total
 
 
 a = Solution()
